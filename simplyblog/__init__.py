c5a45cfe2514bdf8bad365868696172054060608
Ngu0210 <dannynguyen312@gmail.com>
2022-03-09 14:39:30 +0000

    Added Postgres Database

--- simplyblog/__init__.py
+++ simplyblog/__init__.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from dotenv import load_dotenv
+from flask import Flask, jsonify, abort
+import psycopg2
 
-app = Flask(__name__)
+app=Flask(__name__)
+
+load_dotenv() 
+
+connection = psycopg2.connect(
+    database="simply_blog",
+    user="postgres",
+    password="something123",
+    host="44.201.206.16",
+    port="5432"
+)
+
+cursor = connection.cursor()
+
+# cursor.execute("create table if not exists posts (id serial primary key, title varchar);")
+cursor.execute("drop table if exists books;")
+print("sheesh")
+connection.commit()
 
 @app.route("/")
 def hello_world():
-    return "<p>Sheesh!</p>"
+    return "<p>Hello, World!</p>"
 
-@app.route("/stock", methods=["GET"])
-def stock():
-    return "<h1>STOCKS!!!!</h1>"
+@app.route("/nice", methods=["GET"])
+def nice():
+    return "<h1>help</h1>"
