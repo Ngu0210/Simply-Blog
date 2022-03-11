202efb66c917247b8975a54971362dbb8951a1d9
Ngu0210 <dannynguyen312@gmail.com>
2022-03-11 15:45:45 +0000

    Refactor code and added unittesting

--- main.py
+++ main.py
@@ -4,29 +4,37 @@ load_dotenv()
 
 # Init Flask
 from flask import Flask, render_template, jsonify
-app = Flask(__name__) 
-app.config.from_object("default_settings.app_config")
-
-# Init ORM Database
-from database import init_db
-db = init_db(app) 
-
-#Setup Serialization and Deserialization
+from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-ma = Marshmallow(app)
-
-#Setup Marshmallow Validation Handling
-from marshmallow.exceptions import ValidationError
-
-from commands import db_commands
-app.register_blueprint(db_commands)
-
-# Register Controllers
-from controllers import registerable_controllers
-for controller in registerable_controllers:
-    app.register_blueprint(controller)
 
 
-@app.errorhandler(ValidationError)
-def handle_bad_request(error):
-    return (jsonify(error.messages), 400 )
+db = SQLAlchemy()
+ma = Marshmallow()
+
+def create_app():
+    app = Flask(__name__) 
+    app.config.from_object("default_settings.app_config")
+
+    # Init ORM Database
+    db.init_app(app)
+
+    #Setup Serialization and Deserialization
+    ma.init_app(app)
+
+    #Setup Marshmallow Validation Handling
+    from marshmallow.exceptions import ValidationError
+
+    from commands import db_commands
+    app.register_blueprint(db_commands)
+
+    # Register Controllers
+    from controllers import registerable_controllers
+    for controller in registerable_controllers:
+        app.register_blueprint(controller)
+
+
+    @app.errorhandler(ValidationError)
+    def handle_bad_request(error):
+        return (jsonify(error.messages), 400 )
+
+    return app
---
+++ tests/test_user.py
@@ -0,0 +1,5 @@
+import unittest
+
+class TestUser(unittest.TestCase):
+    def test_user_index(self):
+        pass
