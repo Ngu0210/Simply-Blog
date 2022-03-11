62593c407bad31bf50d823545b4de07d3f1145fa
Ngu0210 <dannynguyen312@gmail.com>
2022-03-11 07:40:04 +0000

    Reinstalled project in another EC2

--- commands.py
+++ commands.py
@@ -3,7 +3,37 @@ from main import db
 
 db_commands = Blueprint("db", __name__)
 
-@db_commands.cli.command("dannynguyen")
-def danny():
-    print("Hello world!")
+@db_commands.cli.command("create")
+def create_db():
+    db.create_all()
+    print("Tables Created")
 
+@db_commands.cli.command("drop")
+def create_db():
+    db.drop_all()
+    print("Tables Deleted")
+
+@db_commands.cli.command("seed")
+def seed_db():
+    from models.User import User
+    from faker import Faker
+    faker = Faker()
+
+    for i in range(10):
+        user = User()
+        user.firstname = faker.first_name()
+        user.lastname = faker.last_name()
+
+        db.session.add(user)
+        print(f"{i} user record(s) created")
+
+    db.session.commit()
+    print("Tables seeded")
+    
+
+        db.session.add(user)
+        print(f"{i} user record(s) created")
+
+    db.session.commit()
+    print("Tables seeded")
+    
