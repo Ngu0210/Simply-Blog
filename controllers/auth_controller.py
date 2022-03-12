ba6a985849a7f95fed9e3718899c39208dbecc0e
Ngu0210 <dannynguyen312@gmail.com>
2022-03-12 02:57:18 +0000

    Added User Create Function

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,9 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, request, abort, jsonify
 from schemas.UserSchema import user_schema
 from models.User import User
+from main import db
 
 auth = Blueprint("auth", __name__, url_prefix="/auth")
 
 @auth.route("/register", methods=["POST"])
 def auth_register():
-    return "working"
+    user_fields = user_schema.load(request.json)
+
+    user = User.query.filter_by(email=user_fields["email"]).first()
+
+    if user:
+        return abort(400, description="Email already registered")
+
+    user = User()
+    user.email = user_fields["email"]
+    user.password = user_fields["password"]
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify(user_schema.dump(user))
