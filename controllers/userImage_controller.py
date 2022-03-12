df831a50769c28233d658c062e9008f8fae42783
Ngu0210 <dannynguyen312@gmail.com>
2022-03-12 10:44:05 +0000

    Added userImage controller

--- controllers/userImage_controller.py
+++ controllers/userImage_controller.py
@@ -1,1 +1,26 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, abort
+from flask_jwt_extended import jwt_required
+from services.auth_service import verify_user
+from models.UserImage import UserImage
+from models.User import User
+from schemas.UserImageSchema import userImage_schema
+
+userImages = Blueprint("userImages", __name__, url_prefix="/users/<int:user_id>/image")
+
+@userImages.route("/", methods=["POST"])
+@jwt_required()
+@verify_user
+def userImage_create(user_id, user=None):
+    return "1"
+
+@userImages.route("/<int:id>", methods=["GET"])
+@jwt_required()
+@verify_user
+def userImage_show(user_id, id, user=None):
+    return "2"
+
+@userImages.route("/<int:id>", methods=["DELETE"])
+@jwt_required()
+@verify_user
+def userImage_delete(user_id, user=None):
+    return "3"
